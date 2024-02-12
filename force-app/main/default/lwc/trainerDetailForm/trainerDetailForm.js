0169d5e5665f7dec739269b02359f84fced00cab
ravi750 <rs.csm15@gmail.com>
2024-02-12 23:41:17 +0530

    trainerDetailForm_component created

--- force-app/main/default/lwc/trainerDetailForm/trainerDetailForm.js
+++ force-app/main/default/lwc/trainerDetailForm/trainerDetailForm.js
@@ -1,6 +1,10 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
 
 export default class TrainerDetailForm extends LightningElement {
 
-   
+    @api recordId;
+
+    trainerName;
+
+     // pokemon part-2 ka video 17:00 min ke baad dekhna h
 }
